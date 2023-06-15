236adbea2d1639993703f33174d935cd869bc4e5
Ahmod-1208 <129204162+Ahmod-1208@users.noreply.github.com>
2023-06-15 15:40:48 +0100

    aded

---
+++ JS CLASS/MY PROJECT/E-COMMERCE/app.js
@@ -0,0 +1,32 @@
+const product = [
+    {
+        id: 0,
+        image: '',
+        title: '',
+        price: 200
+    },
+    {
+        id: 1,
+        image: '',
+        title: '',
+        price: 100
+    },
+    {
+        id: 2,
+        image: '',
+        title: '',
+        price: 200
+    },
+    {
+        id:3 ,
+        image: '',
+        title: '',
+        price: 200
+    },
+    {
+        id:4 ,
+        image: '',
+        title: '',
+        price: 210
+    },
+]
